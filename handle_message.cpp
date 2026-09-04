2eee288351507c8dbd9f3b5f1d6a55e021848487
Powopaf <fournier.pa@proton.me>
2026-09-04 17:23:14 +0200

    init on what to do

---
+++ handle_message.cpp
@@ -0,0 +1,11 @@
+/*
+IRC msg:
+
+[:prefix] <command> <param> 	[:trailing]\r\n
+
+must not have a size > 512
+up to 15 param
+command are upper letter or 3 digits
+we don't care about prefix
+:trailing the last param can contain space
+*/
