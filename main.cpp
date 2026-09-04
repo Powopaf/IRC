10531f4acfff4593da26d061ab32a41ae648561f
Mira <lemicara@gmail.com>
2026-09-04 15:15:48 +0200

    deleted old files

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Server.hpp"
-#include "User.hpp"
-
-int main(int ac, char **av){
-    if (ac != 3){
-	    std::cout << "Please format this way: " << av[0] << " <port number> <password>" << std::endl; return 1;}
-	Server server;
-	return 0;
-}
