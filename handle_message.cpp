9e7b3304848f19a69a20432c7037ba575e89896b
PAF <123662077+Powopaf@users.noreply.github.com>
2026-09-08 11:47:16 +0200

    Handle message sent by client (#3)
    * Handle message sent by client
    
    * init on what to do
    
    * add function headeur
    
    * extract cmd from irc msg
    
    * extract args
    
    * finish extract cmd and args
    
    ---------
    
    Co-authored-by: pifourni <pifourni@f1r3s13.paris.42.school>

---
+++ handle_message.cpp
@@ -0,0 +1,61 @@
+#include "handle_message.hpp"
+#include <cctype>
+#include <cstddef>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+
+/*
+IRC msg:
+
+[:prefix] <command> <param> [:trailing]\r\n
+
+must not have a size > 512
+up to 15 param
+command are upper letter or 3 digits
+we don't care about prefix
+:trailing the last param can contain space
+*/
+
+static std::string extract_cmd(std::string msg, size_t i) {
+    std::string cmd;
+    size_t j = 0;
+    while (std::isupper(msg[i])) {
+        cmd[j] = msg[i];
+        i++; j++;
+    }
+    if (msg[i] != ' ')
+        throw std::invalid_argument("The command is invalid" + msg);
+    return cmd;
+}
+
+static std::vector<std::string> extract_args(std::string msg, size_t i) {
+    std::vector<std::string> args;
+    while (msg[i] != '\r') {
+        size_t j = 0;
+        std::string a;
+        while (msg[i] != ' ') {
+            a[j] = msg[i];
+            i++; j++;
+        }
+        args.push_back(a);
+        i++;
+    }
+    return args;
+}
+
+void handle_message(const std::string& msg, std::string cmd, std::vector<std::string> args) {
+    size_t i = 0;
+    if (msg.size() > 512)
+        throw std::length_error("Message is too long");
+    if (msg[msg.size() - 1] != '\n' && msg[msg.size() - 2] != '\r')
+        throw std::invalid_argument("Message does not end with \\r\\n");
+    if (msg[0] == ':') {
+        while (msg[i] != ' ')
+            i++;
+    }
+    i++;
+    cmd = extract_cmd(msg, i);
+    args = extract_args(msg, i);
+}
