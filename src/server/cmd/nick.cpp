978ae9b6859fc468828d81e58ed869789bc06eee
Powopaf <fournier.pa@proton.me>
2026-09-16 15:23:49 +0200

    add logs to nick function

--- src/server/cmd/nick.cpp
+++ src/server/cmd/nick.cpp
@@ -1,4 +1,5 @@
 #include "../../../inc/server/Server.hpp"
+#include <iostream>
 #include <stdexcept>
 
 void Server::nick(std::vector<std::string> args) {
@@ -7,4 +8,5 @@ void Server::nick(std::vector<std::string> args) {
 	if (args[0].empty())
 		throw std::length_error("empty string");
 	_users[uf].setNick(args[0]);
+	std::cout << "Set Nick of: " << _users[uf].getUsername() << " to " << args[0] << std::endl;
 }
