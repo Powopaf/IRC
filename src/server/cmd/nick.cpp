275d57274d0f579921e37cc6d068b9839396df50
Powopaf <fournier.pa@proton.me>
2026-09-16 14:30:22 +0200

    add nick cmd

--- src/server/cmd/nick.cpp
+++ src/server/cmd/nick.cpp
@@ -1,5 +1,10 @@
 #include "../../../inc/server/Server.hpp"
+#include <stdexcept>
 
 void Server::nick(std::vector<std::string> args) {
-
+	if (args.size() != 1)
+		throw std::invalid_argument("expect one arg got != 1");
+	if (args[0].empty())
+		throw std::length_error("empty string");
+	_users[uf].setNick(args[0]);
 }
