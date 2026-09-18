71c240931e0cb498e21b15190f5193d526a323a6
Powopaf <fournier.pa@proton.me>
2026-09-18 16:25:25 +0200

    add split

--- src/server/cmd/join.cpp
+++ src/server/cmd/join.cpp
@@ -1,4 +1,9 @@
 #include "../../../inc/server/Server.hpp"
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <type_traits>
+#include <vector>
 
 /*
 	remove the operator bool from the user.
@@ -11,6 +16,24 @@
 	if there is no args that start by 3  = error
 */
 
+std::vector<std::string> split(std::string input, bool is_channel) {
+	std::vector<std::string> res;
+	std::stringstream stream(input);
+	std::string part;
+	const char delimiter = ',';
+	while (std::getline(stream, part, delimiter)) {
+		if (is_channel && part[0] != '#')
+			throw std::invalid_argument("Channel must have '#' before their name");
+		res.push_back(part);
+	}
+	return res;
+}
+
 void Server::join(std::vector<std::string> args) {
+	std::vector<std::string> channels_name;
+	std::vector<std::string> keys;
+
+	if (args.size() != 1 || args.size() != 2)
+		throw std::invalid_argument("Need 1 or 2 args for JOIN to work");
 
 }
